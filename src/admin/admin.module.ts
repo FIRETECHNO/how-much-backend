30047b2750ee0b06b9970bbfb4cbdbcae91486a5
Jet-Green <grishadzyin@gmail.com>
2025-10-13 18:35:33 +0500

    add reservations history

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import UserModel from 'src/user/models/user.model';
+import JobReservationModel from 'src/job-form/models/job-reservation.model';
 
 @Module({
   controllers: [AdminController],
   providers: [AdminService],
-  imports: [UserModel]
+  imports: [UserModel, JobReservationModel]
 })
 export class AdminModule { }
