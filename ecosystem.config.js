3618cb1dc5c15f5a8e6ed000a71243fd0689065e
Jet-Green <grishadzyin@gmail.com>
2026-04-05 11:59:38 +0500

    cfg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,6 +21,8 @@ module.exports = {
       script: 'max-bot/index.js',
       env: {
         NODE_ENV: 'production',
+        MAX_BOT_TOKEN: process.env.MAX_BOT_TOKEN,
+        CLIENT_URL: process.env.CLIENT_URL,
       },
     },
   ],
